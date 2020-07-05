5e779e612dde89accc448f41fac70817e20893c0
Jose Davalos Rosas <jdavalos@moddtech.com>
2020-07-04 17:11:45 -0700

    Initial Commit

---
+++ GoNetTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoNetTest
+//
+//  Created by José Dávalos Rosas on 04/07/20.
+//  Copyright © 2020 José Dávalos Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
